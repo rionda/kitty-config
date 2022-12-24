8fb8d229786b906600a3787398b5ca5ca6f297f3
Matteo Riondato <rionda@acm.org>
2022-12-24 12:31:06 -0500

    Add w3m alias, and comment about alphabetical order.

--- kittyaliases.zsh
+++ kittyaliases.zsh
@@ -1,4 +1,7 @@
 alias kitty="/Applications/MacPorts/kitty.app/Contents/MacOS/kitty"
+
+# Keep in alphabetical order
 alias icat="kitty +kitten icat"
 alias soldark="kitty +kitten themes --reload-in=all Solarized Dark"
 alias sollight="kitty +kitten themes --reload-in=all Solarized Light"
+alias w3m="w3m -graph -F -o confirm_qq=FALSE -o use_wide=TRUE -o color=TRUE -o auto_image=TRUE -o display_image=TRUE -o max_load_image=20 -o inline_img_protocol=4"
