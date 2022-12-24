33076410dc7f545cf494b16267711ee1f7be331f
Matteo Riondato <rionda@acm.org>
2022-12-24 11:11:37 -0500

    Add icat alias.

--- kittyaliases.zsh
+++ kittyaliases.zsh
@@ -1,3 +1,4 @@
 alias kitty="/Applications/MacPorts/kitty.app/Contents/MacOS/kitty"
+alias icat="kitty +kitten icat"
 alias soldark="kitty +kitten themes --reload-in=all Solarized Dark"
 alias sollight="kitty +kitten themes --reload-in=all Solarized Light"
