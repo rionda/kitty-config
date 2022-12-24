4b67fb162015baf4b5d266a84b4837ac48735eac
Matteo Riondato <rionda@acm.org>
2022-12-24 11:08:56 -0500

    Move my mappings to a separate file.

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git submodule update --init --merge --recursive && \
     mkdir -p ~/.config/kitty && \
     ln -s "${PWD}/kitty-themes" ~/.config/kitty/ && \
     ln -s "${PWD}/kittyaliases.zsh" ~/.config/kitty/ && \
-    ln -s "${PWD}/kitty.conf" ~/.config/kitty/
+    ln -s "${PWD}/*.conf" ~/.config/kitty/
