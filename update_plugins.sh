c061a78c8e1fad5fb827fac1f9cd8e15870fdb6e
Matteo Riondato <rionda@acm.org>
2022-12-24 16:07:36 -0500

    Add update_plugins script.

---
+++ update_plugins.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+(echo "## Pulling from remote" &&  git pull --no-rebase) || exit 1
+(echo "## Syncing submodules " && git submodule sync) || exit 1
+(echo "## Updating/Initing and merging submodules from their remote" &&  \
+    git submodule update --init --remote --merge) || exit 1
+(echo "## Committing update " &&  \
+    git commit --no-gpg-sign .gitmodules kitty-themes -m "Sync submodules.") || \
+    exit 1
+(echo "## Pushing " && git push) || exit 1
