d95573ca2893782b0baf8f10a74a89ad63fbca4a
Matteo Riondato <rionda@acm.org>
2023-01-05 10:12:26 +0100

    Create and link empty current-theme.conf on install.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 git submodule update --init --merge --recursive && \
+    touch current-theme.conf && \
     mkdir -p ~/.config/kitty && \
     ln -s "${PWD}/kitty-themes" ~/.config/kitty/ && \
     ln -s "${PWD}/kittyaliases.zsh" ~/.config/kitty/ && \
