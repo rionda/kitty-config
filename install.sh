11630fec036e1a3161b3dfe477978e4a5257d339
Matteo Riondato <rionda@acm.org>
2023-01-06 11:01:59 +0100

    Just link the whole PWD as the kitty config directory.

--- install.sh
+++ install.sh
@@ -2,7 +2,5 @@
 
 git submodule update --init --merge --recursive && \
     touch current-theme.conf && \
-    mkdir -p ~/.config/kitty && \
-    ln -s "${PWD}/kitty-themes" ~/.config/kitty/ && \
-    ln -s "${PWD}/kittyaliases.zsh" ~/.config/kitty/ && \
-    ln -s "${PWD}/*.conf" ~/.config/kitty/
+    mkdir -p ~/.config && \
+    ln -s "${PWD}" ~/.config/kitty
